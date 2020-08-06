70215f0174d582ac093cba4f720813920b747bc7
Fangdun Cai <cfddream@gmail.com>
2020-08-06 18:48:32 +0800

    feat(viz): session

--- viz/src/middleware/mod.rs
+++ viz/src/middleware/mod.rs
@@ -1,11 +1,13 @@
 mod logger;
 mod recover;
 mod request_id;
+mod session;
 mod timeout;
 
 pub use logger::LoggerMiddleware;
 pub use recover::RecoverMiddleware;
 pub use request_id::RequestIDMiddleware;
+pub use session::SessionMiddleware;
 pub use timeout::TimeoutMiddleware;
 
 pub fn logger() -> LoggerMiddleware {
@@ -22,4 +24,8 @@ pub fn request_id() -> RequestIDMiddleware {
 
 pub fn timeout() -> TimeoutMiddleware {
     TimeoutMiddleware::default()
+}
+
+pub fn session<Store>() -> SessionMiddleware<Store> {
+    SessionMiddleware::default()
 }
