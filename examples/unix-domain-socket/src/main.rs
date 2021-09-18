1652b0e203d92c36c1f4d154b8f008a51d365485
Fangdun Tsai <cfddream@gmail.com>
2021-09-18 15:53:46 +0800

    feat(ci): cirrus

--- examples/unix-domain-socket/src/main.rs
+++ examples/unix-domain-socket/src/main.rs
@@ -23,14 +23,6 @@ async fn main() -> Result<()> {
 }
 
 #[cfg(windows)]
-#[tokio::main]
-async fn main() -> Result<()> {
-    let mut app = viz::new();
-
-    app.routes(router().at("/", route().get(hello)));
-
-    Server::bind(&"127.0.0.1:3000".parse()?)
-        .serve(app.into_make_service())
-        .await
-        .map_err(Error::new)
+fn main()  {
+    println!("please run this example on unix");
 }
