941ab9ceae75a973d944122d7c61ac770d07c54c
Fangdun Tsai <cfddream@gmail.com>
2022-07-14 07:49:24 +0800

    chore(viz): uncomment accept_from_stream

--- viz/src/lib.rs
+++ viz/src/lib.rs
@@ -8,6 +8,6 @@ pub use viz_router::*;
 pub use viz_macros::handler;
 
 // https://github.com/hyperium/hyper/commit/ce72f73464d96fd67b59ceff08fd424733b43ffa#diff-1eaa7c1646ca4a8c2741ab2b4f80d22ab646d8ab031f99925a3adcc3ac242dcd
-// pub use hyper::server::accept::from_stream as accept_from_stream;
+pub use hyper::server::accept::from_stream as accept_from_stream;
 pub use hyper::Server;
 pub use server::{ServiceMaker, Stream};
