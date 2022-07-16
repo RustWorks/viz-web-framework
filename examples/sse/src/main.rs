292aaa6c43c2eb396cf052a9d488041cd7a277ed
Fangdun Tsai <cfddream@gmail.com>
2022-07-16 12:16:23 +0800

    chore(examples): sse - sys stats

--- examples/sse/src/main.rs
+++ examples/sse/src/main.rs
@@ -1,15 +1,16 @@
-// #![deny(warnings)]
+#![deny(warnings)]
 
 use futures_util::StreamExt;
-use std::net::SocketAddr;
-use tokio::time::{interval, Duration, Instant};
+use std::{net::SocketAddr, sync::Arc};
+use systemstat::{Platform, System};
+use tokio::time::{interval, Duration};
 use tokio_stream::wrappers::IntervalStream;
 use viz::{
     get,
     header::ACCEPT,
-    types::{Event, Sse},
-    IntoResponse, Request, RequestExt, Response, ResponseExt, Result, Router, Server, ServiceMaker,
-    StatusCode,
+    types::{Data, Event, Sse},
+    Error, HandlerExt, IntoResponse, Request, RequestExt, Response, ResponseExt, Result, Router,
+    Server, ServiceMaker, StatusCode,
 };
 
 async fn index(_: Request) -> Result<Response> {
@@ -18,15 +19,30 @@ async fn index(_: Request) -> Result<Response> {
     )))
 }
 
-async fn stats(req: Request) -> Result<impl IntoResponse> {
+async fn stats(mut req: Request) -> Result<impl IntoResponse> {
+    // check request `Accept` header
     if !matches!(req.header::<_, String>(ACCEPT), Some(ts) if ts == mime::TEXT_EVENT_STREAM.as_ref())
     {
         Err(StatusCode::BAD_REQUEST.into_error())?
     }
-    let now = Instant::now();
+
+    let Data(sys): Data<Arc<System>> = req.extract().await?;
+
     Ok(Sse::new(
-        IntervalStream::new(interval(Duration::from_secs(10)))
-            .map(move |_| Event::default().data(now.elapsed().as_secs().to_string())),
+        IntervalStream::new(interval(Duration::from_secs(10))).map(move |_| {
+            match sys
+                .load_average()
+                .map_err(|e| Error::Normal(Box::new(e)))
+                .and_then(|loadavg| {
+                    serde_json::to_string(&loadavg).map_err(|e| Error::Normal(Box::new(e)))
+                }) {
+                Ok(loadavg) => Event::default().data(loadavg),
+                Err(err) => {
+                    println!("{}", err);
+                    Event::default().retry(30)
+                }
+            }
+        }),
     )
     .interval(Duration::from_secs(15)))
 }
@@ -36,9 +52,11 @@ async fn main() -> Result<()> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on {}", addr);
 
+    let sys = Arc::new(System::new());
+
     let app = Router::new()
         .route("/", get(index))
-        .route("/stats", get(stats));
+        .route("/stats", get(stats.with(Data::new(sys))));
 
     if let Err(err) = Server::bind(&addr).serve(ServiceMaker::from(app)).await {
         println!("{}", err);
