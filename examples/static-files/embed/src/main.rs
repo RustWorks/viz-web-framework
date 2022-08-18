149dda50a7af8150727c629dcb1cf0326498aa32
Fangdun Tsai <cfddream@gmail.com>
2022-08-18 21:04:14 +0800

    chore(examples): improve embed

--- examples/static-files/embed/src/main.rs
+++ examples/static-files/embed/src/main.rs
@@ -1,7 +1,7 @@
 #![deny(warnings)]
 
 use std::net::SocketAddr;
-use viz::{handlers::embed, IntoResponse, Result, Router, Server, ServiceMaker, StatusCode};
+use viz::{handlers::embed, Result, Router, Server, ServiceMaker, StatusCode};
 
 #[derive(rust_embed::RustEmbed)]
 #[folder = "public"]
@@ -15,9 +15,7 @@ async fn main() -> Result<()> {
     let app = Router::new()
         .get("/", embed::File::<Asset>::new("index.html"))
         .get("/static/*", embed::Dir::<Asset>::default())
-        .any("/*", |_| async {
-            Ok(StatusCode::NOT_FOUND.into_response())
-        });
+        .any("/*", |_| async { Ok(StatusCode::NOT_FOUND) });
 
     if let Err(err) = Server::bind(&addr).serve(ServiceMaker::from(app)).await {
         println!("{}", err);
