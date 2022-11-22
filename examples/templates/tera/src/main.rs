620941d0e7ab77d8fde046ebe84c1fd24c167f6a
Fangdun Tsai <cfddream@gmail.com>
2022-11-22 17:54:01 +0800

    chore(examples): template-tera: add lifetime to User struct

--- examples/templates/tera/src/main.rs
+++ examples/templates/tera/src/main.rs
@@ -15,9 +15,9 @@ static TERA: Lazy<Tera> =
     Lazy::new(|| Tera::new("examples/templates/tera/templates/**/*").unwrap());
 
 #[derive(Serialize)]
-struct User {
-    url: String,
-    username: String,
+struct User<'a> {
+    url: &'a str,
+    username: &'a str,
 }
 
 async fn index(_: Request) -> Result<Response> {
@@ -27,19 +27,18 @@ async fn index(_: Request) -> Result<Response> {
         "users",
         &vec![
             User {
-                url: "https://github.com/rust-lang".to_string(),
-                username: "rust-lang".to_string(),
+                url: "https://github.com/rust-lang",
+                username: "rust-lang",
             },
             User {
-                url: "https://github.com/viz-rs".to_string(),
-                username: "viz-rs".to_string(),
+                url: "https://github.com/viz-rs",
+                username: "viz-rs",
             },
         ],
     );
     let mut buf = BytesMut::with_capacity(512);
     buf.extend(
-        dbg!(&TERA)
-            .render("index.html", &ctx)
+        TERA.render("index.html", &ctx)
             .map_err(Error::normal)?
             .as_bytes(),
     );
