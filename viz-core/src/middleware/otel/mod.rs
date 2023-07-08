acc4813d509f912856f2fb9b07598cebad066fb0
Fangdun Tsai <cfddream@gmail.com>
2023-07-08 09:54:04 +0800

    fix(cargo): resolver

--- viz-core/src/middleware/otel/mod.rs
+++ viz-core/src/middleware/otel/mod.rs
@@ -1,8 +1,8 @@
 //! Opentelemetry request tracking & metrics middleware.
 
+pub(self) const HTTP_HOST: opentelemetry::Key = opentelemetry::Key::from_static_str("http.host");
+
 #[cfg(feature = "otel-metrics")]
 pub mod metrics;
 #[cfg(feature = "otel-tracing")]
 pub mod tracing;
-
-pub(self) const HTTP_HOST: opentelemetry::Key = opentelemetry::Key::from_static_str("http.host");
