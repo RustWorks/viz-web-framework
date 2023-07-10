04082e0fea57b6f079655509dd2bc37ec72d53d5
Fangdun Tsai <cfddream@gmail.com>
2023-07-10 15:54:12 +0800

    test(core): RealIP

--- viz-core/tests/realip.rs
+++ viz-core/tests/realip.rs
@@ -1,8 +1,8 @@
 use http::HeaderValue;
-use viz_core::{header::FORWARDED, types::RealIp, Request, RequestExt, Result};
+use viz_core::{header::FORWARDED, types::RealIp, Request, RequestExt};
 
 #[test]
-fn realip() -> Result<()> {
+fn realip() {
     let mut req = Request::default();
     req.headers_mut()
         .insert(RealIp::X_REAL_IP, HeaderValue::from_static("10.10.10.10"));
@@ -20,5 +20,11 @@ fn realip() -> Result<()> {
         .insert(FORWARDED, HeaderValue::from_static("for=10.10.10.10"));
     assert_eq!(req.realip(), Some(RealIp("10.10.10.10".parse().unwrap())));
 
-    Ok(())
+    let req = Request::default();
+    assert_eq!(req.realip(), None);
+
+    let mut req = Request::default();
+    req.extensions_mut()
+        .insert("1.1.1.1:80".parse::<std::net::SocketAddr>().unwrap());
+    assert_eq!(req.realip(), Some(RealIp("1.1.1.1".parse().unwrap())));
 }
