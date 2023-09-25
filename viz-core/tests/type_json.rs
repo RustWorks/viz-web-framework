d6485cf9cd2a5a5a678295259f47b9476bb22882
Fangdun Tsai <cfddream@gmail.com>
2023-09-25 15:09:05 +0800

    fix: clippy

--- viz-core/tests/type_json.rs
+++ viz-core/tests/type_json.rs
@@ -1,7 +1,7 @@
 use viz_core::{types::Json, IntoResponse, ResponseExt, Result};
 
 #[test]
-fn json() -> Result<()> {
+fn json() {
     let data = Json::new("json");
     let resp = data.into_response();
     assert_eq!(resp.content_type(), Some(mime::APPLICATION_JSON));
@@ -9,6 +9,4 @@ fn json() -> Result<()> {
     let data = Json::new("json");
     let inner = data.clone().into_inner();
     assert_eq!(inner, "json");
-
-    Ok(())
 }
